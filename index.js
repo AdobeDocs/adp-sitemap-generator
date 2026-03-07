40c3b5ee0e0d350062381170ec30018c1b78436d
Diane Batres <dianeba3@gmail.com>
2026-03-06 19:02:50 -0500

    refactor: remove unnecessary XML

--- index.js
+++ index.js
@@ -203,7 +203,6 @@ const main = async () => {
             suppressEmptyNode: true
         });
         const sitemapObj = {
-            '?xml': { _version: '1.0', _encoding: 'UTF-8' },
             urlset: {
                 xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9",
                 url: urls
